7568cb7017bb913d637b4441ae1c3ea0e29aa0c3
jugalkpatel <ptjimmy99@gmail.com>
2022-02-05 13:31:52 +0530

    feat: add Home page and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home } from "./pages";
+import { Trending, SearchResults } from "./components";
+
 function App() {
-  return <h1>Hello,world</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Home />}>
+            <Route index element={<Trending />} />
+            <Route path="/search/:query" element={<SearchResults />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
