1c3ee8e847c214def339b3a365cba4796b12c1d2
jugalkpatel <ptjimmy99@gmail.com>
2022-02-06 01:06:36 +0530

    feat: add ContentContainer in Home Page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
 
 import { Navbar } from "../../components";
+import { ContentContainer } from "../../styles/common.styles";
 
 function Home() {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <ContentContainer>
+        <Outlet />
+      </ContentContainer>
     </>
   );
 }
