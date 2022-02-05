fdc59b0bcb55e39204574f0416dcf1b0b74e69b2
jugalkpatel <ptjimmy99@gmail.com>
2022-02-06 01:07:28 +0530

    feat: add styles in Trending Component

--- src/components/Gif/Gif.tsx
+++ src/components/Gif/Gif.tsx
@@ -3,7 +3,6 @@ import { GIF } from "../../common.types";
 import { GifImg, GifContainer, GifTitle } from "./Gif.styles";
 
 function Gif({ id, img, title }: GIF) {
-  console.log({ title });
   return (
     <GifContainer>
       <GifImg src={img.url} height={img.height} width={img.width} />
--- src/components/Trending/Trending.tsx
+++ src/components/Trending/Trending.tsx
@@ -1,33 +1,67 @@
+import { HashLoader } from "react-spinners";
+
+import {
+  ContentHeader,
+  ErrorIcon,
+  MessageContainer,
+  TrendingIcon,
+  Message,
+} from "../../styles/common.styles";
+
+import { Gif } from "../../components";
 import { useGifs } from "../../hooks";
-import { ACTIONS, STATUS } from "../../utils";
+import { STATUS } from "../../utils";
 
 const URL = process.env.REACT_APP_TRENDING_URL as string;
 const API_KEY = process.env.REACT_APP_API_KEY as string;
 
 function Trending() {
-  const [state, dispatch] = useGifs(URL, {
+  const [state] = useGifs(URL, {
     api_key: API_KEY,
     limit: 10,
     rating: "g",
   });
+  const override = `
+    grid-row: 1 / -1;
+    grid-column: 1 / -1;
+  `;
 
-  if (state.status === STATUS.IDLE || state.status === STATUS.LOADING) {
-    return <h1>Loading.......</h1>;
+  if (state.status === STATUS.LOADING || state.status === STATUS.IDLE) {
+    return <HashLoader loading={true} css={override} size={75} color="#FFF" />;
   }
 
-  if (state.status === STATUS.RESOLVED) {
-    return <pre>{JSON.stringify(state.gifs, null, 2)}</pre>;
+  if (state.status === STATUS.RESOLVED && state.gifs) {
+    if (!state.gifs.length) {
+      return (
+        <MessageContainer>
+          <Message>No GIFs found</Message>
+        </MessageContainer>
+      );
+    }
+
+    return (
+      <>
+        <ContentHeader>
+          <TrendingIcon />
+          Trending
+        </ContentHeader>
+        {state.gifs.map(({ id, img, title }) => {
+          return <Gif key={id} id={id} title={title} img={img} />;
+        })}
+      </>
+    );
   }
 
   if (state.status === STATUS.REJECTED) {
-    return <pre>{JSON.stringify(state.error, null, 2)}</pre>;
+    return (
+      <MessageContainer>
+        <ErrorIcon />
+        <Message>OOPS SOMETHING WENT WRONG PLEASE REFRESH !</Message>
+      </MessageContainer>
+    );
   }
 
-  return (
-    <button onClick={() => dispatch({ type: ACTIONS.SET_LOADING })}>
-      set loading
-    </button>
-  );
+  return <>{null}</>;
 }
 
 export { Trending };
